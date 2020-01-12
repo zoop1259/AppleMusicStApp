7b32879394f3b0fc0fe8c75c296618b7e4867a2b
joonwon lee <cafielo@gmail.com>
2020-01-12 14:05:25 +0900

    플레이어 구현 - 재생 버튼, 시간 표시, 시킹 슬라이드, 컬러

--- AppleMusicStApp/PlayerViewController.swift
+++ AppleMusicStApp/PlayerViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 import AVFoundation
 
 class PlayerViewController: UIViewController {
@@ -15,42 +16,148 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
     
+    @IBOutlet weak var playControlButton: UIButton!
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var totalDurationLabel: UILabel!
+    
     let player = AVPlayer()
+    var timeObserver: Any?
+    var currentTime: Double {
+        return player.currentItem?.currentTime().seconds ?? 0
+    }
+    
+    var totalDurationTime: Double {
+        return player.currentItem?.duration.seconds ?? 0
+    }
+    
+    var isSeeking: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateTintColor()
+        updatePlayButton()
+        updateTime(time: CMTime.zero)
         
-        
-        // Do any additional setup after loading the view.
+        timeObserver = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 10), queue: DispatchQueue.main) { time in
+            self.updateTime(time: time)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         updateTrackInfo()
-        
-        player.play()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        player.pause()
+        player.replaceCurrentItem(with: nil)
+    }
     
+    @IBAction func beginDrag(_ sender: UISlider) {
+        isSeeking = true
+    }
+    
+    @IBAction func endDrag(_ sender: UISlider) {
+        isSeeking = false
+    }
+    
+    @IBAction func seek(_ sender: UISlider) {
+        guard let currentItem = player.currentItem else { return }
+        let position = Double(sender.value)
+        let seconds = currentItem.duration.seconds * position
+        let time = CMTime(seconds: seconds, preferredTimescale: 100)
+        player.seek(to: time)
+    }
+    
+    @IBAction func togglePlayButton(_ sender: UIButton) {
+        if player.isPlaying {
+            player.pause()
+        } else {
+            player.play()
+        }
+        updatePlayButton()
+    }
+}
+
+
+extension PlayerViewController {
     func updateTrackInfo() {
         guard let track = player.currentItem?.convertToTrack() else { return }
-        
         thumbnailImageView.image = track.artwork
         titleLabel.text = track.title
         artistLabel.text = track.artist
-        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateTintColor() {
+        playControlButton.tintColor = DefaultStyle.Colors.tint
+        timeSlider.tintColor = DefaultStyle.Colors.tint
     }
-    */
+    
+    func updateTime(time: CMTime) {
+        // print(time.seconds)
+        // currentTime label, totalduration label, slider
+        currentTimeLabel.text = secondsToString(sec: currentTime)   // 3.1234 >> 00:03
+        totalDurationLabel.text = secondsToString(sec: totalDurationTime)  // 39.2045  >> 00:39
+        
+        if isSeeking == false {
+            timeSlider.value = Float(currentTime/totalDurationTime)
+        }
+    }
+    
+    func secondsToString(sec: Double) -> String {
+        guard sec.isNaN == false else { return "00:00" }
+        let totalSeconds = Int(sec)
+        let min = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        return String(format: "%02d:%02d", min, seconds)
+    }
+    
+    func updatePlayButton() {
+        if player.isPlaying {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 40)
+            let image = UIImage(systemName: "pause.fill", withConfiguration: configuration)
+            playControlButton.setImage(image, for: .normal)
+        } else {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 40)
+            let image = UIImage(systemName: "play.fill", withConfiguration: configuration)
+            playControlButton.setImage(image, for: .normal)
+        }
+    }
+}
 
+extension AVPlayer {
+    var isPlaying: Bool {
+        guard self.currentItem != nil else { return false }
+        return self.rate != 0
+    }
+}
+
+public enum DefaultStyle {
+    
+    public enum Colors {
+        
+        public static let label: UIColor = {
+            if #available(iOS 13.0, *) {
+                return UIColor.label
+            } else {
+                return .black
+            }
+        }()
+        
+        public static let tint: UIColor = {
+            if #available(iOS 13.0, *) {
+                return UIColor { traitCollction in
+                    if traitCollction.userInterfaceStyle == .dark {
+                        return .white
+                    } else {
+                        return .black
+                    }
+                }
+            } else {
+                return .black
+            }
+        }()
+    }
 }
